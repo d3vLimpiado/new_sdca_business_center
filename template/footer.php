7c4bf19479f6cbc90b5d1900800f5faff6fb248c
Joshua Limpiado <jianlimpiado@gmail.com>
2023-12-01 00:30:57 +0800

    chore: development and update of footer and header styles

--- template/footer.php
+++ template/footer.php
@@ -9,6 +9,26 @@
         Saint Dominic Collage Of Asia<br />Business Center
       </span>
     </div>
+    <div class="footer-about">
+      <h3 class="footer-about-title">About</h3>
+      <p class="footer-about-desc">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate dignissimos perferendis temporibus, omnis harum rerum. Voluptates laudantium, nulla quas commodi earum pariatur a exercitationem, veniam fuga error tempora facere sapiente quasi sed vero culpa quaerat provident labore. Magnam rem tempora, adipisci sed maiores labore dolore architecto minus quam! Et, aliquam hic exercitationem ratione officia aut incidunt nemo quibusdam soluta natus est laborum ut obcaecati! Illo distinctio explicabo numquam dolore fuga porro necessitatibus soluta, maxime omnis sint? Accusamus corporis consequatur officia ratione cumque atque sint unde, non consectetur ab, ea facilis placeat ducimus reiciendis repudiandae natus fuga. Nam quisquam ipsa sequi!
+      </p>
+    </div>
+    <div class="footer-social">
+      <a href="#" class="footer-social-container">
+        <i class="fb"></i>
+        <span>Facebook</span>
+      </a href="#">
+      <a href="#" class="footer-social-container">
+        <i class="yt"></i>
+        <span>Youtube</span>
+      </a href="#">
+      <a href="#" class="footer-social-container">
+        <i class="ig"></i>
+        <span>Instagram</span>
+      </a href="#">
+    </div>
   </section>
   <section>
     <span class="copyright">© 2023 by SDCA Students. All rights reserved.</span>
